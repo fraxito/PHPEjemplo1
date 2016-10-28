6a425e4cb1419a2e39cb59ae5b489fccb74148ce
xp <xp@DESKTOP-2D3CPFP>
2016-10-28 17:02:58 +0200

    para los  vagos

--- EjemploQuienEsQuien.php
+++ EjemploQuienEsQuien.php
@@ -10,16 +10,23 @@
         <div class="container" id="centro">
             <div class="row">
                  <div class="col-md-12">
-                     
+                     <div id="posicionY" ></div><br/>
+                     <div id="posicionX" ></div>
+                     <div id="etiqueta01" class="btn btn-primary" style="position: absolute; top:200px; left:100px;">ETIQUETA 1</div>
                  </div>
             </div>
-
-
+        </div>
     </body>
     <script src="js/jquery-3.1.0.min.js"></script>
+    <script src="js/jquery-ui.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script>
-        
+        $('#etiqueta01').draggable({
+            drag: function(){
+                var posicion = $(this).position();
+                $('#posicionY').html(posicion.top);
+            }
+        });
     </script>
     
 </html>
