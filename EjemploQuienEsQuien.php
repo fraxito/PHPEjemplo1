c3e6b038f9fa0db3f7599f7d3c58a5648e304375
xp <xp@DESKTOP-2D3CPFP>
2016-10-28 16:42:27 +0200

    para los fucking vagos

---
+++ EjemploQuienEsQuien.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>PRUEBA quien es quien</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+    </head>
+    <body>
+        <div class="container" id="centro">
+            <div class="row">
+                 <div class="col-md-12">
+                     
+                 </div>
+            </div>
+
+
+    </body>
+    <script src="js/jquery-3.1.0.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script>
+        
+    </script>
+    
+</html>
+
+
+
+
+<?php
+
+
+
